cd7b895ba017da0a56901f52260636b4b10eadda
Vaishnavi-honnimani <01fe24bca154@kletech.ac.in>
2025-10-23 04:38:27 -0700

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,8 +1,13 @@
 
+
 salary = float(input("Enter your salary: ₹"))
 bonus = salary * 0.10
 total_salary = salary + bonus
+tax = total_salary * 0.05
+final_salary = total_salary - tax
 
 print(f"Original Salary: ₹{salary}")
 print(f"Bonus (10%): ₹{bonus}")
-print(f"Total Salary after Bonus: ₹{total_salary}")
+print(f"Salary after Bonus: ₹{total_salary}")
+print(f"Tax Deducted (5%): ₹{tax}")
+print(f"Final Salary after Tax: ₹{final_salary}")
