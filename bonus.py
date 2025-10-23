2f4f7358b3a2f645c7f670dbec5f4c19b86c9169
vaishnavi-honnimani <01fe24bca154@kletech.ac.in>
2025-10-23 04:34:18 -0700

    save

---
+++ bonus.py
@@ -0,0 +1,8 @@
+
+salary = float(input("Enter your salary: ₹"))
+bonus = salary * 0.10
+total_salary = salary + bonus
+
+print(f"Original Salary: ₹{salary}")
+print(f"Bonus (10%): ₹{bonus}")
+print(f"Total Salary after Bonus: ₹{total_salary}")
